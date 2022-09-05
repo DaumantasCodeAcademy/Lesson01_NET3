bf792d396e6d68a4fe30744cb1407b6d41ef8981
DaumaG <dgarnys18@gmail.com>
2022-09-05 20:40:03 +0300

    Add project files.

---
+++ Lesson01_01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson01_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kokia Jūsų mėgstamiausia spalva?");
+            Console.ReadLine();
+            Console.WriteLine($"Mano irgi!");
+        }
+    }
+}
